93dd21383a26d16a2965fc426bc125c0aba675a7
Samuel Croker <samuel.croker@gmail.com>
2016-03-04 12:36:55 -0500

    Generalization
    generalized for uploading

--- readGeocode.R
+++ readGeocode.R
@@ -1,12 +1,15 @@
 library(RCurl)
 library(RJSONIO)
+library(RCurl)
 
+#preapre the data to send to google for geocoding
 construct.geocode.url <- function(address, return.call = "json", sensor = "false") {
   root <- "http://maps.google.com/maps/api/geocode/"
   u <- paste(root, return.call, "?address=", address, "&sensor=", sensor, sep = "")
   return(URLencode(u))
 }
 
+#funciton that takes the address, sends to google and saves results, looking at whether the returned codes are accurate or if there was a problem
 gGeoCode <- function(address,verbose=FALSE) {
   if(verbose) cat(address,"\n")
   u <- construct.geocode.url(address)
@@ -23,7 +26,7 @@ gGeoCode <- function(address,verbose=FALSE) {
   }
 }
 
-houses <- read.csv(file='C:\\Users\\scroker\\Documents\\R\\Geo\\Charleston.csv',stringsAsFactors=F)
+houses <- read.csv(file='Charleston.csv',stringsAsFactors=F)
 
 
 hcoord <- t(apply(as.data.frame(houses$Address), 1, gGeoCode))
@@ -47,21 +50,6 @@ while(length(bad[,1]>0)){
   print(paste("Iteration:",i," NumBad:",length(bad[,1])))
 }  
 print(paste("Original:",length(houses[,1])," Final",length(good[,1])))
-
-
-# 
-# hcoord2 <- t(apply(as.data.frame(bad$Address), 1, gGeoCode))
-# colnames(hcoord2) <- c('Latitude','Longitude')
-# fin2 <- cbind(bad[,c(1:5)],hcoord2)
-# 
-# good2 <- subset(fin2,!is.na(fin2$Latitude))
-# bad2 <- subset(fin2,is.na(fin2$Latitude))
-# 
-# hcoord3 <- t(apply(as.data.frame(bad2$Address), 1, gGeoCode))
-# colnames(hcoord3) <- c('Latitude','Longitude')
-# fin3 <- cbind(bad2[,c(1:5)],hcoord3)
-# 
-# final <- rbind(good,good2,fin3)
 
 # write out the results to be grabbed by Tableau
 write.csv(good,file='C:\\Users\\scroker\\Documents\\R\\Geo\\CharlestonGeo.csv')
